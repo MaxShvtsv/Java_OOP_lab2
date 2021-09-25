e7d336ac1fa0d7ea2936742d56f73e95e7d662bb
MaxN <maks.nimchenko@gmail.com>
2021-09-25 15:46:52 +0300

    Fixed bug: RegEx like char delimiters.

--- Calculator.java
+++ Calculator.java
@@ -17,33 +17,81 @@ public class Calculator{
         printLine();
         int a = calc.Add("");
         System.out.println(a); // 0
+        if (a == 0){
+            System.out.println("True");
+        }
         printLine();
         int b = calc.Add("1");
         System.out.println(b); // 1
+        if (b == 1){
+            System.out.println("True");
+        }
         printLine();
         int c = calc.Add("1,210");
         System.out.println(c); // 211
+        if (c == 211){
+            System.out.println("True");
+        }
         printLine();
         int d = calc.Add("5\n100,10");
         System.out.println(d); // 115
+        if (d == 115){
+            System.out.println("True");
+        }
         printLine();
         int e = calc.Add("//abc\n10,100\n50abc60");
         System.out.println(e); // 220
+        if (e == 220){
+            System.out.println("True");
+        }
         printLine();
         int f = calc.Add("//::\n-10::100\n-50::60");
         System.out.println(f); // 0, Exception, with negatives.
+        if (f == 0){
+            System.out.println("True");
+        }
         printLine();
         int g = calc.Add("//::\n10::2021\n50::60");
         System.out.println(g); // 120
+        if (g == 120){
+            System.out.println("True");
+        }
         printLine();
         int h = calc.Add("//[aaaaa]\n10aaaaa2021,50aaaaa0");
         System.out.println(h); // 60
+        if (h == 60){
+            System.out.println("True");
+        }
         printLine();
         int i = calc.Add("//[a][b][c]\n10a20b50c10,100");
         System.out.println(i); // 190
+        if (i == 190){
+            System.out.println("True");
+        }
         printLine();
         int j = calc.Add("//[aaa][bb][cccc]\n100aaa5bb105cccc6,0");
         System.out.println(j); // 216
+        if (j == 216){
+            System.out.println("True");
+        }
+        printLine();
+        int test1 = calc.Add("//[*][**][***]\n2*7**8***10");
+        System.out.println(test1); // 27
+        if (test1 == 27){
+            System.out.println("True");
+        }
+        printLine();
+        int test2 = calc.Add("//[a][**][+++]\n5*5**12+++12");
+        System.out.println(test2); // 34
+        if (test2 == 34){
+            System.out.println("True");
+        }
+        printLine();
+        int test3 = calc.Add("//[ ][**][+++]\n5 5 12 10");
+        System.out.println(test3); // 32
+        if (test3 == 32){
+            System.out.println("True");
+        }
         printLine();
     }
 
@@ -54,6 +102,8 @@ public class Calculator{
 
 /* Class of string calculator. */
 class StringCalculator{
+
+    String regexSymbols[] = {"*", "+", "?", "\\", ".", "^", "$", "$", "|"};
 
     /* Summarize digits in given string.
      * numbers: string, string consists of digits and delimiters.
@@ -83,6 +133,9 @@ class StringCalculator{
                     //           from user delimiter.
                     int end;
 
+                    // isRegexDelimiter: boolean, determines if there is RegEx reserved chars.
+                    Boolean isRegexDelimiter = true;
+
                     // Set end var. through the loop.
                     for (end = start; numbers.charAt(end) != '\n'; end++);
 
@@ -95,21 +148,45 @@ class StringCalculator{
 
                     numbers = numbers.substring(end + 1, numbers.length());
 
+                    String gotDelimiter = multipleLengthDelimiter.substring(1, multipleLengthDelimiter.length() - 1);
+                    
+                    int delimiterLen = gotDelimiter.length();
+
+                    // Process RegEx reserved delimiters.
+                    for (int i = 0; i < delimiterLen; i++){
+                        for (String regexChar : regexSymbols){
+                            if (regexChar.equals(String.valueOf(gotDelimiter.charAt(i)))){
+                                isRegexDelimiter = false;
+                                if (i == 0){
+                                    gotDelimiter = "/" + gotDelimiter;
+                                } else{
+                                    gotDelimiter = gotDelimiter.substring(0, i) + "/" + gotDelimiter.substring(i);
+                                }
+                                delimiterLen += 1;
+                                i += 1;
+                            }
+                        }
+                    }
+
+                    gotDelimiter = "[" + gotDelimiter + "]";
+
                     if (bracketsCount == 1){
                         // Multilength delimiter.
                         
                         // Concatinate delimiters.
-                        delimiters += "|" + multipleLengthDelimiter.substring(1, multipleLengthDelimiter.length() - 1);
+                        delimiters += "|" + gotDelimiter;
 
                     } else{
                         // Multiple delimiters.
-
-                        multipleLengthDelimiter = multipleLengthDelimiter.substring(1, multipleLengthDelimiter.length() - 1);
-
-                        String arrayOfDelimiters[] = multipleLengthDelimiter.split("\\]\\[");
-
-                        for (String delimiter : arrayOfDelimiters){
-                            delimiters += "|" + delimiter;
+                        if (!isRegexDelimiter){
+                            // We don't remove brackets if there RegEx like delimiter string.
+                            multipleLengthDelimiter = gotDelimiter;
+    
+                            String arrayOfDelimiters[] = multipleLengthDelimiter.split("\\]\\[");
+    
+                            for (String delimiter : arrayOfDelimiters){
+                                delimiters += "|" + delimiter;
+                            }
                         }
                     }
                 }else{
@@ -134,9 +211,9 @@ class StringCalculator{
                     delimiters += "|" + userDelimiter;
                 }
             }
-
             
             // SplittedDigits: string array, array of splitted digits.
+
             String splittedDigits[] = numbers.split(delimiters);
 
             // digitsCount: int, count of digits in the string.
@@ -146,11 +223,16 @@ class StringCalculator{
             int intDigitsArray[] = new int[digitsCount];
 
             Boolean isNegativeExist = false;
-
             for (int i = 0; i < digitsCount; i++){
 
                 // currentNumber: int, number to be processed.
-                int currentNumber = Integer.parseInt(splittedDigits[i]);
+                int currentNumber = 0;
+
+                try {
+                    currentNumber = Integer.parseInt(splittedDigits[i]);
+                } catch (Exception Ex) {
+                    currentNumber = 0;
+                }
 
                 if (currentNumber > 1000){
                     // Numbers bigger then 1000 are ignored.
